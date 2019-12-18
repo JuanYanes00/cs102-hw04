fcbf53670eadc8fc07fb6cabe68e9c26250f512b
Juan Yanes <juan.yanesbenatuil@cooper.edu>
2019-12-18 17:00:18 -0400

    Beautiful reddish filter

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ new_img = Image.new("RGB", (width, height), "white")
 
 # TODO: Replace this with your own filter!
 # Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [(0, 0)] * 9
+
 for i in range(1, width - 1):
-    for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        members.sort()
-        new_img.putpixel((i, j), (members[4]))
+    for j in range(1, height -1):
+        r, g, b = img.getpixel((i,j))
+        if (g+50>255 or b-50<0):
+            r=int(g)
+            g=int(b)
+            b=int(r/5)
+        else:
+            r = int(g + 50)
+            g = int(b - 50)
+            b = int(r / 5)
+
+        new_img.putpixel((i, j), (r,g,b))
 
 new_img.save(output_path, "JPEG")
